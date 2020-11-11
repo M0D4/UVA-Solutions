3e91fb36a6b585754a5fb2a94ad06fc171491879
Moustafa Mohamed <darsh.m7md@gmail.com>
2020-11-11 02:43:18 +0200

    added AC solution

---
+++ 11631/main.cpp
@@ -0,0 +1,114 @@
+#include "bits/stdc++.h"
+
+#pragma GCC optimize("Ofast")
+
+
+using namespace std;
+
+#define sc              scanf
+#define endl            "\n"
+#define sz(x)          (int) (x.size())
+#define all(v)          v.begin(), v.end()
+#define isOdd(n)       (n&1)
+//#define pow(n, m)      (ll)powl(n, m)
+#define clr(x, val)     memset(x, val, sizeof(x))
+#define numOfDigits(x)  (x ? (ll)(log10(x)) + 1 : 1)
+
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> tii;
+typedef vector<int> vi;
+
+
+
+struct DSU{
+
+    const static int N = 2e5 + 5; //nodes
+
+    int parent[N], groupSize[N];
+    int groupCnt;
+
+    DSU(int n){
+        groupCnt = n;
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+            groupSize[i] = 1;
+        }
+    }
+
+    int findLeader(int i){
+        if(parent[i] == i) return i;
+
+        return parent[i] = findLeader(parent[i]);
+    }
+
+    bool sameGroup(int x, int y){
+        return findLeader(x) == findLeader(y);
+    }
+
+    void mergeGroups(int x, int y){//merge to one component
+        int leaderX = findLeader(x);
+        int leaderY = findLeader(y);
+
+        if(leaderX == leaderY) return;
+
+        groupCnt--;
+
+        if(groupSize[leaderX] > groupSize[leaderY]){
+            parent[leaderY] = leaderX;
+            groupSize[leaderX] += groupSize[leaderY];
+        }else{
+            parent[leaderX] = leaderY;
+            groupSize[leaderY] += groupSize[leaderX];
+        }
+    }
+
+    int getSize(int x){//size of group
+        return groupSize[findLeader(x)];
+    }
+};
+
+
+
+int n, m; //nodes, edges
+
+void Kruskal(){
+
+    DSU dsu(n + 2);
+
+
+
+    vector<tuple<int, int, int>> edgeList(m);
+
+    int MST = 0, totalEdges = 0;
+
+    for(int i = 0; i < m; i++){
+        int u, v, w; cin >> u >> v >> w;
+        totalEdges += w;
+        edgeList[i] = tie(w, u, v);
+    }
+
+    sort(all(edgeList));//sort by w
+
+
+    for(int i = 0; i < m; i++){
+        double w; int u, v; tie(w, u, v) = edgeList[i];
+        if(!dsu.sameGroup(u, v)){//will not cause a cycle
+            dsu.mergeGroups(u, v);
+            MST += w;
+        }
+    }
+    cout << totalEdges - MST << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    while(cin >> n >> m && n){
+        Kruskal();
+    }
+    return 0;
+}
